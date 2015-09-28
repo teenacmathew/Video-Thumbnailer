8996a0029514b655e111cda74d3554730041f6c1
teena <teena@qburst.com>
2015-09-28 10:25:05 +0530

    video_resize

---
+++ video_resize_gem/lib/video_resize_gem.rb
@@ -0,0 +1,16 @@
+module VideoResizeGem
+
+  class Uploader
+
+    require "carrierwave"
+    require "mini_magick"
+    require "rmagick"
+
+
+    def self.upload?
+      puts "YOU ARE AWESOME!!"
+    end
+
+  end
+
+end
