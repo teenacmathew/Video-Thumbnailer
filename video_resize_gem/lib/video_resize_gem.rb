1e15072bd148ff5bf7ecccd07ce80feba3e270ac
teena <teena@qburst.com>
2015-10-05 11:43:30 +0530

    included carrierwave

--- video_resize_gem/lib/video_resize_gem.rb
+++ video_resize_gem/lib/video_resize_gem.rb
@@ -2,6 +2,14 @@ module VideoResizeGem
 
   class Uploader
 
+    require "carrierwave"
+    require "mini_magick"
+    require "rmagick"
+
+    include CarrierWave
+    include MiniMagick
+    include RMagick
+
     def self.upload?
       puts "YOU ARE AWESOME!!"
     end
