b513d685ddfd6a5958d62dcbc7ab0141bcb852b6
teena <teena@qburst.com>
2015-10-13 13:58:54 +0530

    thumbnail

---
+++ lib/ffmpeg_thumbnail.rb
@@ -0,0 +1,62 @@
+require 'open3'
+module FfmpegThumbnail
+	class Thumbnail
+	  attr_reader :input_path, :output_path
+
+	  def initialize(in_path, out_path)
+	  	@input_path = in_path
+	  	@output_path = out_path
+	  end
+
+	  def generate_command options = {}
+	  	cmd = %Q(ffmpeg -i #{input_path})
+	  	# Adding Rotation
+ 		  cmd += set_rotation(options[:rotate]) if options[:rotate]
+	  	cmd += %Q( -ss #{options[:time]}) if options[:time]
+	  	# Adding thumbnail size
+	  	cmd += %Q( -s #{options[:width]}x#{options[:height]}) if options[:height] && options[:width]
+	  	# cmd += %Q( -vf fps=1/120 )
+	  	cmd += %Q( -vframes 1  #{output_path+options[:file_name]}.#{options[:format]})
+	  	p cmd
+      exit_code = nil
+      error = nil
+	  	raise Errno::ENOENT unless File.exist?(@input_path)
+
+      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|			  
+			  error =  stderr.read
+			  exit_code = wait_thr.value
+			end
+      handle_exit_code(exit_code, error)
+	  # rescue Errno::ENOENT
+	  # 	puts "file not found"
+	  end
+
+	  private
+
+	  	def handle_exit_code(exit_code, error)
+        if exit_code == 0
+          # logger.info("Success!")
+          puts "Success!"
+        else
+         	puts error
+          # logger.error("Failure!")
+          puts "Failure!"
+        end
+        exit_code
+      end
+
+      def set_rotation(degree)
+      	 case degree
+		  	 when 90
+		  	 	%Q( -vf "transpose=1")
+		  	 when 180
+		  	 	%Q( -vf vflip )
+		  	 when 270
+		  	 	%Q( -vf "transpose=2")
+		  	 else
+		  	 		" "		  			
+		  	 end 
+      end
+  end
+end
+
---
+++ lib/video_thumbnailer/version.rb
@@ -0,0 +1,3 @@
+module VideoThumbnailer
+  VERSION = "0.1.0"
+end
