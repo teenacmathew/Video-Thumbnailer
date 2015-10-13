2f043fc8a542b001ce3ae82de432f53bd2a44322
teena <teena@qburst.com>
2015-10-13 15:59:51 +0530

    thumbnail

--- lib/video_thumbnailer.rb
+++ lib/video_thumbnailer.rb
@@ -1,8 +1,7 @@
 require "video_thumbnailer/version"
-
-
 require 'open3'
-module FfmpegThumbnail
+
+ module FfmpegThumbnail
 	class Thumbnail
 	  attr_reader :input_path, :output_path
 
@@ -14,39 +13,34 @@ module FfmpegThumbnail
 	  def generate_command options = {}
 	  	cmd = %Q(ffmpeg -i #{input_path})
 	  	# Adding Rotation
- 		  cmd += set_rotation(options[:rotate]) if options[:rotate]
+ 		cmd += set_rotation(options[:rotate]) if options[:rotate]
 	  	cmd += %Q( -ss #{options[:time]}) if options[:time]
 	  	# Adding thumbnail size
 	  	cmd += %Q( -s #{options[:width]}x#{options[:height]}) if options[:height] && options[:width]
 	  	# cmd += %Q( -vf fps=1/120 )
-	  	cmd += %Q( -vframes 1  #{output_path}.#{options[:format]})
+	  	cmd += %Q( -vframes 1  #{output_path})
 	  	p cmd
-      exit_code = nil
-      error = nil
+     	 exit_code = nil
+      	error = nil
 	  	raise Errno::ENOENT unless File.exist?(@input_path)
-
-      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|			  
+      	Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|			  
 			  error =  stderr.read
 			  exit_code = wait_thr.value
 			end
-      handle_exit_code(exit_code, error)
-	  # rescue Errno::ENOENT
-	  # 	puts "file not found"
+      	handle_exit_code(exit_code, error)
 	  end
 
 	  private
 
 	  	def handle_exit_code(exit_code, error)
-        if exit_code == 0
-          # logger.info("Success!")
-          puts "Success!"
-        else
-         	puts error
-          # logger.error("Failure!")
-          puts "Failure!"
-        end
-        exit_code
-      end
+         if exit_code == 0
+           puts "Success!"
+         else
+           puts error
+           puts "Failure!"
+         end
+         exit_code
+     	 end
 
       def set_rotation(degree)
       	 case degree
@@ -58,26 +52,23 @@ module FfmpegThumbnail
 		  	 	%Q( -vf "transpose=2")
 		  	 else
 		  	 		" "		  			
-		  	 end 
-      end
+		  end 
+    end
   end
 end
 
 module  VideoThumbnailer 
 include FfmpegThumbnail
 
- def generate_thumb(param_video,input_path,output_path)
-  puts "hi"
+ def generate_thumb(param_video)
   tmp_path = File.join( File.dirname(current_path), "tmpfile.jpg" )
   a  = Thumbnail.new(current_path, tmp_path)
   a.generate_command(param_video)
   p "********************************"
-
-  p  "#{tmp_path}.jpg"
+  p  "#{tmp_path}"
   p  current_path
-
-  File.rename  "#{tmp_path}.jpg", current_path
-  puts "hello"
- end
+  p "********************************"
+  File.rename  "#{tmp_path}", current_path
+  end
 
 end
