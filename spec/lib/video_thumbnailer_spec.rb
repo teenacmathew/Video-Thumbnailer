8fe802a60a0eea61b9e583331d02d14ea333a62a
teena <teena@qburst.com>
2015-10-16 14:30:21 +0530

    rspec

--- spec/lib/video_thumbnailer_spec.rb
+++ spec/lib/video_thumbnailer_spec.rb
@@ -1,20 +1,12 @@
 require 'spec_helper'
 require 'video_thumbnailer'
 
-RSpec.describe VideoThumbnailer do
+RSpec.describe "generate_thumb" do
+  include VideoThumbnailer
 
-  RSpec.describe "generate_thumb" do
-
-    it "runs with options" do
-      opts = {
-        :height => "40",
-        :width => "20"
-      }
-
-      options = VideoThumbnailer.instance_methods opts
-      expect(options).to eq([:generate_thumb ])
-    end
-
-  end
+  # it "runs with options" do
+  #   generatethumb = generate_thumb({:size => "40" })
+  #   expect(generatethumb).to eq("")
+  # end
 
 end
